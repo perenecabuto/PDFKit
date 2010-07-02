c61f8d683c2894c2cfecf08b43707c63514c4c9f
Felipe Ramos <perenecabuto@gmail.com>
2010-07-02 01:09:25 -0300

    Correction to middleware works fine with rails 2.3 and to not depend on generic .erb views

--- lib/pdfkit/middleware.rb
+++ lib/pdfkit/middleware.rb
@@ -1,63 +1,65 @@
 class PDFKit
-  
   class Middleware
-    
+
     def initialize(app, options = {})
       @app = app
       @options = options
     end
-        
+
     def call(env)
       @render_pdf = false
-      set_request_to_render_as_pdf(env) if env['PATH_INFO'].match(/\.pdf$/)
-      
+
+      if env['PATH_INFO'].match(/\.pdf$/)
+        set_request_to_render_as_pdf(env)
+        env['PATH_INFO'].sub!(/\.pdf$/, '')
+        env['REQUEST_URI'].sub!(/\.pdf$/, '')
+      end
+
       status, headers, response = @app.call(env)
-      
+
       request = Rack::Request.new(env)
       if @render_pdf && headers['Content-Type'] =~ /text\/html|application\/xhtml\+xml/
         body = response.body
-        
         body = translate_paths(body, env)
-        
+
         pdf = PDFKit.new(body, @options)
         body = pdf.to_pdf
-        
+
         # Do not cache PDFs
         puts "DELETING CACHING"
         headers.delete('ETag')
         headers.delete('Cache-Control')
-        
+
         headers["Content-Length"] = body.bytes.to_a.size.to_s
         headers["Content-Type"] = "application/pdf"
-        
+
         response = [body]
       end
-      
+
       [status, headers, response]
     end
-    
+
     private
-    
-      # Change relative paths to absolute
-      def translate_paths(body, env)
-        # Host with protocol
-        root = env['rack.url_scheme'] + "://" + env['HTTP_HOST'] + "/"
-        
-        body.gsub!(/(href|src)=['"]\/([^\"']*|[^"']*)['"]/,'\1="'+root+'\2"')
-      end
-    
-      def set_request_to_render_as_pdf(env)
-        @render_pdf = true
-        puts "Setting PDF mode"
-        
-        path = Pathname(env['PATH_INFO'])
-        env['PATH_INFO'] = path.to_s.sub(/#{path.extname}$/,'') if path.extname == '.pdf'
-        env['HTTP_ACCEPT'] = concat(env['HTTP_ACCEPT'], Rack::Mime.mime_type('.html'))
-      end
-      
-      def concat(accepts, type)
-        (accepts || '').split(',').unshift(type).compact.join(',')
-      end
-  
+
+    # Change relative paths to absolute
+    def translate_paths(body, env)
+      # Host with protocol
+      root = env['rack.url_scheme'] + "://" + env['HTTP_HOST'] + "/"
+
+      body.gsub!(/(href|src)=['"]\/([^\"']*|[^"']*)['"]/,'\1="'+root+'\2"')
+    end
+
+    def set_request_to_render_as_pdf(env)
+      @render_pdf = true
+      puts "Setting PDF mode"
+
+      path = Pathname(env['PATH_INFO'])
+      env['PATH_INFO'] = path.to_s.sub(/#{path.extname}$/,'') if path.extname == '.pdf'
+      env['HTTP_ACCEPT'] = concat(env['HTTP_ACCEPT'], Rack::Mime.mime_type('.html'))
+    end
+
+    def concat(accepts, type)
+      (accepts || '').split(',').unshift(type).compact.join(',')
+    end
   end
 end
